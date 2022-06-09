712d35a6ce6ba66559de3ec771d7ed78b404a48b
Zeveke Marina <99339038+Ehrlich02@users.noreply.github.com>
2022-06-09 20:21:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,56 +4,47 @@
 
 
 bool checkPrime(uint64_t value) {
-  setlocale(LC_ALL, "Russian");
-  cout << "Введите число: ";
-  cin >> value;
-  int i=2;
-  while (i*i<=value) {
-    if (value%i==0)
-      return false;
-    i++; }
-  return true;
+// вставьте код функции
+    int a;
+    if (value < 2)
+            return 0;
+    for (a = 2; a <= value; a++) {
+        if (value%a == 0)
+            if (value != a)
+                return 0;
+    }
+    return 1;
 }
 
 uint64_t nPrime(uint64_t n) {
-  setlocale(LC_ALL, "Russian");
-  int n, i, j, a;
-  cout << "Введите номер числа в ряду: ";
-  cin >> n;
-  a=0; i-2; j=2;
-  for (i=2; ;i++) {
-    for (j=2; j*j<=i; j++) {
-      if (i%j==0) {
+// вставьте код функции
+    int a = 0, c = 0;
+    while (true) {
         a++;
-        if (a==n) {
-          cout<<i;
-          break; }
-      }
+        if (checkPrime(a))
+            c++;
+        if (c == n)
+            return a;
     }
-  }
-  system("Pause");
-  return i;
+    return c;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  setlocale(LC_ALL, "Russian");
-  cout << "Введите число: ";
-  cin >> value;
-  i=0; j=2;
-  for (i=value+1;;i++) {
-    while(j*j<=i) {
-      if (i%j==0)
-        break;
-    j++; }
-  }
-  return i;
+// вставьте код функции
+    int val = value+1;
+    while (true) {
+        if (checkPrime(val))
+            return val;
+        val++;
+    }
+    return val;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int sum=0;
-  for(int i=2; i<hbound; i++) {
-    if (checkPrime(i))
-      sum += i;
-  }
-  return sum;
+// вставьте код функции
+    uint64_t s = 0, a;
+    for (a = 0; a < hbound; a++)
+    if (checkPrime(a))
+        s += a;
+    return s;
 }
