ae1b1159e726ef6cd331a7584a1f9fb4affa483b
Zeveke Marina <99339038+Ehrlich02@users.noreply.github.com>
2022-06-09 20:36:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,46 +5,42 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-    int a;
-    if (value < 2)
-            return 0;
-    for (a = 2; a <= value; a++) {
-        if (value%a == 0)
-            if (value != a)
-                return 0;
-    }
-    return 1;
+uint64_t p = 2;
+while (p * p <=value) {
+  if (value % p == 0) return false;
+  else
+    p++;
+}
+  return true;// вставьте код функции
 }
 
 uint64_t nPrime(uint64_t n) {
+uint64_t p = 1, value{};
+  for (int i = 1; i <= n; i++) {
+    while (1) {
+            p++;
+            value = checkPrime(p);
+            if (value) break;
+         }
+     }
+     return p;
 // вставьте код функции
-    int a = 0, c = 0;
-    while (true) {
-        a++;
-        if (checkPrime(a))
-            c++;
-        if (c == n)
-            return a;
-    }
-    return c;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-    int val = value+1;
-    while (true) {
-        if (checkPrime(val))
-            return val;
-        val++;
+uint64_t n = 1;
+  while (!checkPrime(value+n)) {
+      n++;
     }
-    return val;
-}//
+    return value+n;// вставьте код функции
+}
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-    uint64_t s = 0, a;
-    for (a = 0; a < hbound; a++)
-    if (checkPrime(a))
-        s += a;
-    return s;
+uint64_t sum = 0;
+  for (int i = 2; i < hbound; i++) {
+    if (checkPrime(i)) sum += i;
+}
+    return sum;// вставьте код функции
 }
